16311d0ae9868ec84e5637f1fd6095ec60b77a57
hiyoriaya <hiyoriaya@gmail.com>
2016-01-28 15:30:49 +0900

    変換機能のみ完成

--- app/src/main/java/com/example/hiyoriaya/grongi/MainActivity.java
+++ app/src/main/java/com/example/hiyoriaya/grongi/MainActivity.java
@@ -70,172 +70,271 @@ public class MainActivity extends Activity implements View.OnTouchListener,View.
         String[] chars = new String[gtxt.length()];
 
         for(int i=0;i<chars.length;i++){
-            if(String.valueOf(gtxt.charAt(i))=="あ") {
+            if(String.valueOf(gtxt.charAt(i)).equals("あ")) {
                 chars[i] = "ガ";
-            }else if(String.valueOf(gtxt.charAt(i))=="い"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("い")){
                 chars[i] = "ギ";
-            }else if(String.valueOf(gtxt.charAt(i))=="う"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("う")){
                 chars[i] = "グ";
-            }else if(String.valueOf(gtxt.charAt(i))=="え"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("え")){
                 chars[i] = "ゲ";
-            }else if(String.valueOf(gtxt.charAt(i))=="お"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("お")){
                 chars[i] = "ゴ";
-            }else if(String.valueOf(gtxt.charAt(i))=="か"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("か")){
                 chars[i] = "バ";
-            }else if(String.valueOf(gtxt.charAt(i))=="き"){
-                chars[i] = "ビ";
-            }else if(String.valueOf(gtxt.charAt(i))=="く"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("き")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "キャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "キュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "キョ";
+                }else {
+                    chars[i] = "ビ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("く")){
                 chars[i] = "ブ";
-            }else if(String.valueOf(gtxt.charAt(i))=="け"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("け")){
                 chars[i] = "ベ";
-            }else if(String.valueOf(gtxt.charAt(i))=="こ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("こ")){
                 chars[i] = "ボ";
-            }else if(String.valueOf(gtxt.charAt(i))=="さ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("さ")){
                 chars[i] = "ガ";
-            }else if(String.valueOf(gtxt.charAt(i))=="し"){
-                chars[i] = "ギ";
-            }else if(String.valueOf(gtxt.charAt(i))=="す"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("し")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "シャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "シュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ショ";
+                }else {
+                    chars[i] = "ギ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("す")){
                 chars[i] = "グ";
-            }else if(String.valueOf(gtxt.charAt(i))=="せ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("せ")){
                 chars[i] = "ゲ";
-            }else if(String.valueOf(gtxt.charAt(i))=="そ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("そ")){
                 chars[i] = "ゴ";
-            }else if(String.valueOf(gtxt.charAt(i))=="た"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("た")){
                 chars[i] = "ダ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ち"){
-                chars[i] = "ヂ";
-            }else if(String.valueOf(gtxt.charAt(i))=="つ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ち")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "チャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "チュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "チョ";
+                }else {
+                    chars[i] = "ヂ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("つ")){
                 chars[i] = "ヅ";
-            }else if(String.valueOf(gtxt.charAt(i))=="て"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("て")){
                 chars[i] = "デ";
-            }else if(String.valueOf(gtxt.charAt(i))=="と"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("と")){
                 chars[i] = "ド";
-            }else if(String.valueOf(gtxt.charAt(i))=="な"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("な")){
                 chars[i] = "バ";
-            }else if(String.valueOf(gtxt.charAt(i))=="に"){
-                chars[i] = "ビ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぬ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("に")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ニャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ニュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ニョ";
+                }else {
+                    chars[i] = "ビ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぬ")){
                 chars[i] = "ブ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ね"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ね")){
                 chars[i] = "ベ";
-            }else if(String.valueOf(gtxt.charAt(i))=="の"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("の")){
                 chars[i] = "ボ";
-            }else if(String.valueOf(gtxt.charAt(i))=="は"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("は")){
                 chars[i] = "ザ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ひ"){
-                chars[i] = "ジ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ふ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ひ")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ヒャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ヒュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ヒョ";
+                }else {
+                    chars[i] = "ジ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ふ")){
                 chars[i] = "ズ";
-            }else if(String.valueOf(gtxt.charAt(i))=="へ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("へ")){
                 chars[i] = "ゼ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ほ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ほ")){
                 chars[i] = "ゾ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ま"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ま")){
                 chars[i] = "ラ";
-            }else if(String.valueOf(gtxt.charAt(i))=="み"){
-                chars[i] = "リ";
-            }else if(String.valueOf(gtxt.charAt(i))=="む"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("み")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ミャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ミュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ミョ";
+                }else {
+                    chars[i] = "リ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("む")){
                 chars[i] = "ル";
-            }else if(String.valueOf(gtxt.charAt(i))=="メ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("め")){
                 chars[i] = "レ";
-            }else if(String.valueOf(gtxt.charAt(i))=="モ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("も")){
                 chars[i] = "ロ";
-            }else if(String.valueOf(gtxt.charAt(i))=="や"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("や")){
                 chars[i] = "シャ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ゆ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ゆ")){
                 chars[i] = "シュ";
-            }else if(String.valueOf(gtxt.charAt(i))=="よ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("よ")){
                 chars[i] = "ショ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ら"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ら")){
                 chars[i] = "サ";
-            }else if(String.valueOf(gtxt.charAt(i))=="り"){
-                chars[i] = "シ";
-            }else if(String.valueOf(gtxt.charAt(i))=="る"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("り")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "リャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "リュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "リョ";
+                }else {
+                    chars[i] = "シ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("る")){
                 chars[i] = "ス";
-            }else if(String.valueOf(gtxt.charAt(i))=="れ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("れ")){
                 chars[i] = "セ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ろ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ろ")){
                 chars[i] = "ソ";
-            }else if(String.valueOf(gtxt.charAt(i))=="わ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("わ")){
                 chars[i] = "パ";
-            }else if(String.valueOf(gtxt.charAt(i))=="を") {
+            }else if(String.valueOf(gtxt.charAt(i)).equals("を")) {
                 chars[i] = "ゾ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ん"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ん")){
                 chars[i] = "ン";
-            }else if(String.valueOf(gtxt.charAt(i))=="が"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("が")){
                 chars[i] = "ガ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぎ"){
-                chars[i] = "ギ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぐ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぎ")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ギャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ギュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ギョ";
+                }else {
+                    chars[i] = "ギ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぐ")){
                 chars[i] = "グ";
-            }else if(String.valueOf(gtxt.charAt(i))=="げ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("げ")){
                 chars[i] = "ゲ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ご"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ご")){
                 chars[i] = "ゴ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ざ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ざ")){
                 chars[i] = "ザ";
-            }else if(String.valueOf(gtxt.charAt(i))=="じ"){
-                chars[i] = "ジ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ず"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("じ")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ジャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ジュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ジョ";
+                }else {
+                    chars[i] = "ジ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ず")){
                 chars[i] = "ズ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぜ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぜ")){
                 chars[i] = "ゼ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぞ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぞ")){
                 chars[i] = "ゾ";
-            }else if(String.valueOf(gtxt.charAt(i))=="だ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("だ")){
                 chars[i] = "ザ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぢ"){
-                chars[i] = "ジ";
-            }else if(String.valueOf(gtxt.charAt(i))=="づ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぢ")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ヂャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ヂュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ヂョ";
+                }else {
+                    chars[i] = "ジ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("づ")){
                 chars[i] = "ズ";
-            }else if(String.valueOf(gtxt.charAt(i))=="で"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("で")){
                 chars[i] = "ゼ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ど"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ど")){
                 chars[i] = "ゾ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ば"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ば")){
                 chars[i] = "ダ";
-            }else if(String.valueOf(gtxt.charAt(i))=="び") {
-                chars[i] = "ヂ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぶ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("び")) {
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ビャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ビュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ビョ";
+                }else {
+                    chars[i] = "ヂ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぶ")){
                 chars[i] = "ヅ";
-            }else if(String.valueOf(gtxt.charAt(i))=="べ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("べ")){
                 chars[i] = "デ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぼ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぼ")){
                 chars[i] = "ド";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぱ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぱ")){
                 chars[i] = "マ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぴ"){
-                chars[i] = "ミ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぷ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぴ")){
+                if(String.valueOf(gtxt.charAt(i+1)).equals("ゃ")){
+                    chars[i] = "ミャ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ゅ")){
+                    chars[i] = "ミュ";
+                }else if(String.valueOf(gtxt.charAt(i+1)).equals("ょ")){
+                    chars[i] = "ミョ";
+                }else {
+                    chars[i] = "ミ";
+                }
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぷ")){
                 chars[i] = "ム";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぺ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぺ")){
                 chars[i] = "メ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぽ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぽ")){
                 chars[i] = "モ";
-            }else if(String.valueOf(gtxt.charAt(i))=="ゃ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ゃ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ゅ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ゅ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ょ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ょ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぁ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぁ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぃ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぃ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぅ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぅ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぇ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぇ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ぉ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ぉ")){
                 chars[i] = "";
-            }else if(String.valueOf(gtxt.charAt(i))=="ー"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("ー")){
                 chars[i] = chars[i-1];
-            }else if(String.valueOf(gtxt.charAt(i))=="っ"){
+            }else if(String.valueOf(gtxt.charAt(i)).equals("っ")){
                 chars[i] = chars[i+1];
+            }else{
+                chars[i] = "";
             }
         }
+        gtxt = "";
         for(int i=0;i<chars.length;i++){
-            gtxt += chars[i];
+            gtxt = gtxt+ chars[i];
         }
         result.setText(gtxt);
     }

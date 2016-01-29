1fe17a80528765ac8e6c44612310b06103081fd8
hiyoriaya <hiyoriaya@gmail.com>
2016-01-29 13:43:12 +0900

    翻訳モード実装。Howtoボタン実装。

--- app/src/main/java/com/example/hiyoriaya/grongi/MainActivity.java
+++ app/src/main/java/com/example/hiyoriaya/grongi/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.hiyoriaya.grongi;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v4.app.ShareCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableStringBuilder;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -23,8 +26,10 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
     private EditText input;
     private Switch mode;
     private ImageView share;
+    private ImageView howto;
     private InputMethodManager imm;
     private boolean getmode;
+    private PopupWindow mpopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,8 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
         mode.setOnCheckedChangeListener(this);
         share = (ImageView) findViewById(R.id.share);
         share.setOnTouchListener(this);
+        howto = (ImageView)findViewById(R.id.howto);
+        howto.setOnTouchListener(this);
         getmode = false;
     }
 
@@ -55,7 +62,11 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
         if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_ENTER) {
             userm.setText(input.getText());
             imm.hideSoftInputFromWindow(input.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
-            grongi();
+            if(getmode) {
+                ghonyaku();
+            }else{
+                ghenkan();
+            }
             input.setText("");
             return true;
         }
@@ -64,21 +75,234 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(MainActivity.this);
-        builder.setText(result.getText().toString());
-        builder.setType("text/plain");
-        builder.startChooser();
+        switch (v.getId()) {
+            case R.id.share :
+                ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(MainActivity.this);
+                builder.setText(result.getText().toString());
+                builder.setType("text/plain");
+                builder.startChooser();
+                break;
+
+            case R.id.howto:
+
+                mpopupWindow = new PopupWindow(MainActivity.this);
+                View popupView = getLayoutInflater().inflate(R.layout.popup_layout,null);
+                mpopupWindow.setContentView(popupView);
+                mpopupWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.popup_background));
+                mpopupWindow.setOutsideTouchable(true);
+                mpopupWindow.setFocusable(true);
+
+                float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
+                mpopupWindow.setWindowLayoutMode((int) width, WindowManager.LayoutParams.WRAP_CONTENT);
+                mpopupWindow.setWidth((int) width);
+                mpopupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+
+                mpopupWindow.showAtLocation(findViewById(R.id.input), Gravity.CENTER, 0, 0);
+
+                break;
+        }
         return false;
     }
 
-    public void grongi() {
+    @Override
+    protected void onDestroy(){
+        if(mpopupWindow != null && mpopupWindow.isShowing()){
+            mpopupWindow.dismiss();
+        }
+        super.onDestroy();
+    }
+
+    public void ghonyaku(){
         String gtxt;
         gtxt = input.getText().toString();
         String[] chars = new String[gtxt.length()];
-        if (getmode) { //翻訳
+        int[] pilen = new int[gtxt.length()];
+        for (int i =0;i<chars.length;i++) {
+            if (i!=chars.length-1) {
+                if (String.valueOf(gtxt.charAt(i)).equals(gtxt.charAt(i+1))){
+                    chars[i + 1] = "ー";
+                }
+            }
+            if (i!=0) {
+                if (String.valueOf(gtxt.charAt(i)).equals(String.valueOf(gtxt.charAt(i - 1)))){
+                    chars[i-1] += "(っ)";
+                }
+            }
+            if (String.valueOf(gtxt.charAt(i)).equals("ガ")) {
+                chars[i] = "(あ・が・さ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ギ")) {
+                chars[i] = "(い・ぎ・し)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("グ")) {
+                chars[i] = "(う・ぐ・す)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ゲ")) {
+                chars[i] = "(え・げ・せ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ゴ")) {
+                chars[i] = "(お・ご・そ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ザ")) {
+                chars[i] = "(ざ・だ・は)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ジ")) {
+                chars[i] = "(じ・ぢ・ひ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ズ")) {
+                chars[i] = "(ず・づ・ふ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ゼ")) {
+                chars[i] = "(ぜ・で・へ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ゾ")) {
+                chars[i] = "(ぞ・ど・ほ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ダ")) {
+                chars[i] = "(た・ば)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ヂ")) {
+                chars[i] = "(ち・び)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ヅ")) {
+                chars[i] = "(つ・ぶ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("デ")) {
+                chars[i] = "(て・べ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ド")) {
+                chars[i] = "(と・ぼ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("バ")) {
+                chars[i] = "(か・な)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ビ")) {
+                chars[i] = "(き・に)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ブ")) {
+                chars[i] = "(く・ぬ)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ベ")) {
+                chars[i] = "(け・ね)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ボ")) {
+                chars[i] = "(こ・の)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("サ")) {
+                chars[i] = "ら";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("シ")) {
+                chars[i] = "り";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ス")) {
+                chars[i] = "る";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("セ")) {
+                chars[i] = "れ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ソ")) {
+                chars[i] = "ろ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("マ")) {
+                chars[i] = "ぱ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ミ")) {
+                chars[i] = "ぴ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ム")) {
+                chars[i] = "ぷ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("メ")) {
+                chars[i] = "ぺ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("モ")) {
+                chars[i] = "ぽ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ラ")) {
+                chars[i] = "ま";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("リ")) {
+                chars[i] = "み";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ル")) {
+                chars[i] = "む";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("レ")) {
+                chars[i] = "め";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ロ")) {
+                chars[i] = "も";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("パ")) {
+                chars[i] = "(わ・は)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ン")) {
+                chars[i] = "(ん・の)";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ャ")) {
+                if (String.valueOf(gtxt.charAt(i - 1)).equals("キ")) {
+                    chars[i - 1] = "キ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ギ")) {
+                    chars[i - 1] = "ギ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("シ")) {
+                    chars[i - 1] = "シ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ジ")) {
+                    chars[i - 1] = "ジ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("チ")) {
+                    chars[i - 1] = "チ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヂ")) {
+                    chars[i - 1] = "ヂ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ニ")) {
+                    chars[i - 1] = "ニ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヒ")) {
+                    chars[i - 1] = "ヒ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ビ")) {
+                    chars[i - 1] = "ビ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ミ")) {
+                    chars[i - 1] = "ミ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ピ")) {
+                    chars[i - 1] = "ピ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("リ")) {
+                    chars[i - 1] = "リ";
+                }
+                chars[i] = "ゃ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ュ")) {
+                if (String.valueOf(gtxt.charAt(i - 1)).equals("キ")) {
+                    chars[i - 1] = "キ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ギ")) {
+                    chars[i - 1] = "ギ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("シ")) {
+                    chars[i - 1] = "シ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ジ")) {
+                    chars[i - 1] = "ジ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("チ")) {
+                    chars[i - 1] = "チ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヂ")) {
+                    chars[i - 1] = "ヂ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ニ")) {
+                    chars[i - 1] = "ニ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヒ")) {
+                    chars[i - 1] = "ヒ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ビ")) {
+                    chars[i - 1] = "ビ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ミ")) {
+                    chars[i - 1] = "ミ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ピ")) {
+                    chars[i - 1] = "ピ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("リ")) {
+                    chars[i - 1] = "リ";
+                }
+                chars[i] = "ゅ";
+            } else if (String.valueOf(gtxt.charAt(i)).equals("ョ")) {
+                if (String.valueOf(gtxt.charAt(i - 1)).equals("キ")) {
+                    chars[i - 1] = "キ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ギ")) {
+                    chars[i - 1] = "ギ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("シ")) {
+                    chars[i - 1] = "シ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ジ")) {
+                    chars[i - 1] = "ジ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("チ")) {
+                    chars[i - 1] = "チ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヂ")) {
+                    chars[i - 1] = "ヂ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ニ")) {
+                    chars[i - 1] = "ニ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ヒ")) {
+                    chars[i - 1] = "ヒ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ビ")) {
+                    chars[i - 1] = "ビ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ミ")) {
+                    chars[i - 1] = "ミ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("ピ")) {
+                    chars[i - 1] = "ピ";
+                } else if (String.valueOf(gtxt.charAt(i - 1)).equals("リ")) {
+                    chars[i - 1] = "リ";
+                }
+                chars[i] = "ょ";
+            } else {
+                chars[i] = String.valueOf(gtxt.charAt(i));
+            }
+        }
+        gtxt = "";
+        for (int i = 0; i < chars.length; i++) {
+            gtxt = gtxt + chars[i];
+        }
+        result.setText(gtxt);
+    }
 
-        } else {    //変換
+    public void ghenkan() {
+        String gtxt;
+        gtxt = input.getText().toString();
+        String[] chars = new String[gtxt.length()];
+        int[] pilen = new int[gtxt.length()];
             for (int i = 0; i < chars.length; i++) {
+                if (String.valueOf(gtxt.charAt(i)).equals("っ")) {
+                    pilen[i] = 1;
+                }
                 if (String.valueOf(gtxt.charAt(i)).equals("あ")) {
                     chars[i] = "ガ";
                 } else if (String.valueOf(gtxt.charAt(i)).equals("い")) {
@@ -333,13 +557,15 @@ public class MainActivity extends Activity implements View.OnTouchListener, View
                     chars[i] = "";
                 } else if (String.valueOf(gtxt.charAt(i)).equals("ぉ")) {
                     chars[i] = "";
-                } else if (String.valueOf(gtxt.charAt(i)).equals("ー")) {
+                } else if (String.valueOf(gtxt.charAt(i)).equals("ー") && i!=0) {
                     chars[i] = chars[i - 1];
-                } else if (String.valueOf(gtxt.charAt(i)).equals("っ")) {
-                    chars[i] = chars[i + 1];
                 } else {
                     chars[i] = String.valueOf(gtxt.charAt(i));
                 }
+            }
+        for(int i = 0; i<chars.length;i++){
+            if(pilen[i] == 1 && i!=chars.length-1){
+                chars[i] = chars[i+1];
             }
         }
         gtxt = "";
